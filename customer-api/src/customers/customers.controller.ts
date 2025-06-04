cd5801177d2ba609beedd1df5f01b209939ea0ac
Bob Adelmann <bobwares@users.noreply.github.com>
2025-06-04 16:45:24 -0500

    Add NestJS customer API

---
+++ customer-api/src/customers/customers.controller.ts
@@ -0,0 +1,43 @@
+// App: Customer CRUD Application
+// Package: customer-api
+// File: src/customers/customers.controller.ts
+// Version: 2.0.30
+// Author: Bobwares
+// Date: 2025-06-04 21:28:20 UTC
+// Description: HTTP controller for customer routes.
+//
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import { CustomersService } from './customers.service';
+import { CreateCustomerDto } from './dto/create-customer.dto';
+import { UpdateCustomerDto } from './dto/update-customer.dto';
+
+@Controller('customers')
+export class CustomersController {
+  constructor(private readonly customersService: CustomersService) {}
+
+  @Post()
+  create(@Body() dto: CreateCustomerDto) {
+    return this.customersService.create(dto);
+  }
+
+  @Get()
+  findAll() {
+    return this.customersService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.customersService.findOne(Number(id));
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() dto: UpdateCustomerDto) {
+    return this.customersService.update(Number(id), dto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    this.customersService.remove(Number(id));
+    return {};
+  }
+}
---
+++ customer-api/src/customers/customers.module.ts
@@ -0,0 +1,17 @@
+// App: Customer CRUD Application
+// Package: customer-api
+// File: src/customers/customers.module.ts
+// Version: 2.0.30
+// Author: Bobwares
+// Date: 2025-06-04 21:28:20 UTC
+// Description: Module defining the customers feature.
+//
+import { Module } from '@nestjs/common';
+import { CustomersService } from './customers.service';
+import { CustomersController } from './customers.controller';
+
+@Module({
+  controllers: [CustomersController],
+  providers: [CustomersService],
+})
+export class CustomersModule {}
